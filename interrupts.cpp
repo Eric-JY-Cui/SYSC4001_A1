4d0b5af3c9875945c28aa81e9d516702361c6fd0
Eric Cui <goldengodzilla304@gmail.com>
2025-09-25 10:57:15 -0400

    Code Functionality
    implemented the basic code. will add more trace files later

--- interrupts.cpp
+++ interrupts.cpp
@@ -3,12 +3,13 @@
  * @file interrupts.cpp
  * @author Sasisekhar Govind
  *
+ * source build.sh
+ * ./bin/interrupts
  */
 
 #include<interrupts.hpp>
 
 int main(int argc, char** argv) {
-
     //vectors is a C++ std::vector of strings that contain the address of the ISR
     //delays  is a C++ std::vector of ints that contain the delays of each device
     //the index of these elemens is the device number, starting from 0
@@ -19,18 +20,49 @@ int main(int argc, char** argv) {
     std::string execution;  //!< string to accumulate the execution output
 
     /******************ADD YOUR VARIABLES HERE*************************/
-
+    int time = 0;
 
 
     /******************************************************************/
 
     //parse each line of the input trace file
     while(std::getline(input_file, trace)) {
+        //std::cout << execution << std::endl;
         auto [activity, duration_intr] = parse_trace(trace);
 
         /******************ADD YOUR SIMULATION CODE HERE*************************/
 
+        
 
+
+        if (activity == "CPU")
+        {
+            execution.append(std::to_string(time) + ", " + std::to_string(duration_intr) + ", CPU execution\n");
+            time = time + duration_intr;
+        }
+        else if (activity == "SYSCALL")
+        {
+            execution.append(std::to_string(time) + ", " + "1" + ", Switch to kernel mode\n");
+            time = time + 1;
+            execution.append(std::to_string(time) + ", " + "4" + ", context saved\n");
+            time = time + 4;
+            execution.append(std::to_string(time) + ", " + "1" + ", find vector " + std::to_string(duration_intr) + 
+                    " in memory " + vectors[duration_intr] + "\n");
+            time = time + 1;
+            execution.append(std::to_string(time) + ", " + "1" + ", obtain ISR address\n");
+            time = time + 1;
+            execution.append(std::to_string(time) + ", " + "40" + ", call device driver\n");
+            time = time + delays[duration_intr];
+            execution.append(std::to_string(time) + ", " + "1" + ", IRET\n");
+            time = time + 1;
+            
+        }
+        else if (activity == "END_IO")
+        {
+            execution.append(std::to_string(time) + ", " + std::to_string(delays[duration_intr]) + ", end of I/O "+ std::to_string(duration_intr)+" interrupt\n");
+            time = time + duration_intr;
+            
+        }
 
         /************************************************************************/
 
